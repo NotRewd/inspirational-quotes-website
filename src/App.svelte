926f1127f4e78b12daf6e8f95e807f4e334b53df
NotRewd <48103943+NotRewd@users.noreply.github.com>
2024-07-11 15:44:54 +0200

    Initial website configuration

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<div class="main">
+  <h1>Inspirational Quotes</h1>
+  <p class="heading-description">
+    Get your inspirational quote to make your day better.
+  </p>
+
+  <button class="button">Get Quote</button>
+</div>

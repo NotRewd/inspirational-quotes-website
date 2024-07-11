223e0e63ad718c4b1a3d29842f32f0dac5e318b3
NotRewd <48103943+NotRewd@users.noreply.github.com>
2024-07-11 17:50:43 +0200

    Added background and started implementing functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,26 @@
+<script>
+  let quote = "Loading a quote...";
+
+  const generateQuote = async () => {
+    const res = await fetch("https://favqs.com/api/qotd");
+    const data = await res.json();
+    quote = `"${data.quote.body.text}" - ${data.quote.author}`;
+  };
+</script>
+
+<img
+  src="./public/background-image.png"
+  id="background-image"
+  alt="background"
+/>
+
 <div class="main">
   <h1>Inspirational Quotes</h1>
   <p class="heading-description">
     Get your inspirational quote to make your day better.
   </p>
 
-  <button class="button">Get Quote</button>
+  <p class="quote">{quote}</p>
+
+  <button class="button" on:click={generateQuote}>Get Quote</button>
 </div>
